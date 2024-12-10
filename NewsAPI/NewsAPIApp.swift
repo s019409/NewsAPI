61d7089495114914ffb61e8dd36146e6617d5c24
Lucy Thoen <s019409@students.lmsd.org>
2024-12-10 14:35:50 -0500

    Initial Commit

---
+++ NewsAPI/NewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAPIApp.swift
+//  NewsAPI
+//
+//  Created by Lucille Thoen (student LM) on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
